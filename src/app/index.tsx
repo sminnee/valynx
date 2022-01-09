5922cb00a0540b906c200e5ba90c284a9a24d134
Sam Minnée <sam.minnee@gmail.com>
2022-01-09 14:43:04 +1300

    feat: Introduce projects as well as people to start building out join/related data features

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { Box, Grid, Paper, TextField } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box, Paper, TextField } from "@mui/material";
+import { DataGrid, GridColumns, GridSelectionModel } from "@mui/x-data-grid";
 import { createFromReactState, ValueLink } from "valynx";
 import "./index.css";
 
@@ -11,11 +11,19 @@ type Person = {
   id: number;
   name: string;
   email: string;
+  projects: number[];
+};
+
+type Project = {
+  id: number;
+  name: string;
+  description: string;
 };
 
 type AppState = {
-  data: Person[];
-  current: number | null;
+  people: Person[];
+  projects: Project[];
+  current: GridSelectionModel;
 };
 
 /**
@@ -46,40 +54,50 @@ const PersonDetail = (props: { state: ValueLink<Person> }) => {
 };
 
 /**
- * The list of people.
- * Full AppState is passed, as we need to deal with both the items and the currently selected item.
- * @param props Control
+ * Detail form for editing a project
  */
-const PersonList = (props: { state: ValueLink<AppState> }) => {
-  // props.state.edata nad props.state.current return more value links, and we call .value and .set() on them directly
-
-  const state = props.state; //.props();
-
+const ProjectDetail = (props: { state: ValueLink<Project> }) => {
+  const state = props.state.props();
   return (
     <Paper className="fill-height">
-      <DataGrid
-        columns={[
-          { field: "name", width: 200 },
-          { field: "email", width: 200 },
-        ]}
-        rows={state.prop("data").value}
-        onRowClick={(person) => state.prop("current").set(person.row.id)}
-      />
+      <LinkedField label="Name" state={state.name} />
+      <LinkedField label="Description" state={state.description} />
     </Paper>
   );
 };
+
+/**
+ * The list of either people or projects
+ */
+function DataList<T>(props: { data: ValueLink<T[]>; selected: ValueLink<GridSelectionModel>; columns: GridColumns }) {
+  return (
+    <Paper className="fill-height">
+      <DataGrid
+        rows={props.data.value}
+        selectionModel={props.selected.value}
+        onSelectionModelChange={props.selected.set}
+        columns={props.columns}
+      />
+    </Paper>
+  );
+}
 
 /**
  * Our simple test app
  */
 const App = () => {
   const reactState = React.useState<AppState>(() => ({
-    data: [
-      { id: 1000, name: "Sam", email: "sam@example.com" },
-      { id: 1001, name: "Ingo", email: "ingo@example.com" },
-      { id: 1002, name: "Ben", email: "ben@example.com" },
+    people: [
+      { id: 1000, name: "Sam", email: "sam@example.com", projects: [2000, 2001] },
+      { id: 1001, name: "Ingo", email: "ingo@example.com", projects: [2001, 2002] },
+      { id: 1002, name: "Ben", email: "ben@example.com", projects: [2002] },
     ],
-    current: null,
+    projects: [
+      { id: 2000, name: "Fun", description: "Things that are fun" },
+      { id: 2001, name: "Chores", description: "Things that are boring but you don't get paid for" },
+      { id: 2002, name: "Work", description: "Things that pay" },
+    ],
+    current: [],
   }));
 
   // Valynx is easy to use with a useRate result
@@ -88,23 +106,49 @@ const App = () => {
   // Find the current person, returns a value link to a person or null, making it easy to conditionally show the form below
   // state.data.find(predicate) returns a value link to the record matching that predicate, or null. Updates to the result
   // will be saved back into the global state
-  const currentPersonId = state.prop("current").value;
-  const currentPerson = currentPersonId ? state.prop("data").find((row) => row.id === currentPersonId) : null;
+  // Relies at the moment on no overlap between project & person IDs
+  const currentId = state.prop("current").item(0).value;
+  const currentPerson = currentId ? state.prop("people").find((row) => row.id === currentId) : null;
+  const currentProject = currentId ? state.prop("projects").find((row) => row.id === currentId) : null;
 
   return (
-    <Box sx={{ display: "grid", height: "100%", gridTemplateRows: "80px auto" }}>
-      <Box>
+    <Box
+      sx={{
+        display: "grid",
+        height: "100%",
+        gridTemplateRows: "38px auto 38px auto",
+        gridTemplateColumns: "49% auto",
+        gap: "24px",
+      }}>
+      <Box sx={{ gridColumn: "1", gridRow: "1" }}>
         <h1>People</h1>
       </Box>
-      <Box>
-        <Grid container item spacing={3} className="fill-height">
-          <Grid item xs={12} sm={6}>
-            <PersonList state={state} />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            {currentPerson && <PersonDetail state={currentPerson} />}
-          </Grid>
-        </Grid>
+      <Box sx={{ gridColumn: "1", gridRow: "2" }}>
+        <DataList
+          data={state.prop("people")}
+          selected={state.prop("current")}
+          columns={[
+            { field: "name", width: 200 },
+            { field: "email", width: 200 },
+          ]}
+        />
+      </Box>
+      <Box sx={{ gridColumn: "1", gridRow: "3" }}>
+        <h1>Projects</h1>
+      </Box>
+      <Box sx={{ gridColumn: "1", gridRow: "4" }}>
+        <DataList
+          data={state.prop("projects")}
+          selected={state.prop("current")}
+          columns={[
+            { field: "name", width: 200 },
+            { field: "description", width: 200 },
+          ]}
+        />
+      </Box>
+      <Box sx={{ gridColumn: "2", gridRowStart: "2", gridRowEnd: "5" }}>
+        {currentPerson && <PersonDetail state={currentPerson} />}
+        {currentProject && <ProjectDetail state={currentProject} />}
       </Box>
     </Box>
   );
